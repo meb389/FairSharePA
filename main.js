1236f390fdff5b04261ef4504d50cfaeb608434e
Mitch <41233950+meb389@users.noreply.github.com>
2018-11-07 13:29:56 -0500

    Uploaded latest versions of HTML, CSS, JS

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.add("show");
 }
+
 // Close the dropdown menu if the user clicks outside of it
 window.addEventListener("click", function(event) {
     if (!event.target.matches('.dropBtn')) {
-        let dropdowns = document.getElementsByClassName("dropdownContent");
-        let i;
+        var dropdowns = document.getElementsByClassName("dropdownContent");
+        var i;
         for (i = 0; i < dropdowns.length; i++) {
             let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
         }
-    }});
+    }
+});
 
-
-
+//Close the dropdown menu on mobile devices when user clicks outside of it
+window.addEventListener("touchmove", function(event) {
+    if (!event.target.matches('.dropBtn')) {
+        var dropdowns = document.getElementsByClassName("dropdownContent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+});
 
 //______________________________________________________________________________________________________________________
 //______________________________________________________________________________________________________________________
